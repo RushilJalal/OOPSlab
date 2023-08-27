c82746fbde32a7d102bf28271c49e29f89ec4463
Rushil Jalal <rushiljalal@gmail.com>
2023-08-27 23:25:45 +0530

    completed magicsquare

--- lab3&4/src/main/java/Lab4/Add_Multiply_matrix.java
+++ lab3&4/src/main/java/Lab4/Add_Multiply_matrix.java
@@ -9,8 +9,9 @@ package Lab4;
  * @author student
  */
 import java.util.*;
-public class Add_Multiply_matrix {  
-        public static void main(String[] args) {
+
+public class Add_Multiply_matrix {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter dimensions of matrix 1: ");
         int m = sc.nextInt();
@@ -23,54 +24,51 @@ public class Add_Multiply_matrix {
         int A[][] = new int[m][n];
         int P[][] = new int[m][q];
         System.out.println("Enter elements of arr1");
-        for(int i = 0; i < m; i++) {
-            for( int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 arr1[i][j] = sc.nextInt();
             }
         }
         System.out.println("Enter elements of arr2");
-        for(int i = 0; i < p; i++) {
-            for(int j = 0; j < q; j++) {
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
                 arr2[i][j] = sc.nextInt();
             }
         }
-        //check whether addition possible
-        if ((m == p)&&(n == q)) {
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n; j++) {
+        // check whether addition possible
+        if ((m == p) && (n == q)) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     A[i][j] = arr1[i][j] + arr2[i][j];
+                }
             }
-        }
             System.out.println("Printing addition result: ");
-            for(int i = 0; i < p; i++) {
-            for(int j = 0; j < q; j++) {
-                System.out.print(A[i][j] + " ");
-            }
+            for (int i = 0; i < p; i++) {
+                for (int j = 0; j < q; j++) {
+                    System.out.print(A[i][j] + " ");
+                }
                 System.out.println("");
-        }
-        }
-        else 
-                System.out.println("Addition not possible");
-        
-        //check whether multiplication possible
-        if(n==p) {
-            for(int i=0;i<m;i++){    
-            for(int j=0;j<q;j++){     
-            for(int k=0;k<n;k++)      
-            {      
-            P[i][j]+=arr1[i][k]*arr2[k][j];
             }
-}
-}
+        } else
+            System.out.println("Addition not possible");
+
+        // check whether multiplication possible
+        if (n == p) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < q; j++) {
+                    for (int k = 0; k < n; k++) {
+                        P[i][j] += arr1[i][k] * arr2[k][j];
+                    }
+                }
+            }
             System.out.println("Printing multiplication result: ");
-            for(int i = 0; i < p; i++) {
-            for(int j = 0; j < q; j++) {
-                System.out.print(P[i][j] + " ");
-            }
+            for (int i = 0; i < p; i++) {
+                for (int j = 0; j < q; j++) {
+                    System.out.print(P[i][j] + " ");
+                }
                 System.out.println("");
-        }
-        }
-        else
-                System.out.println("Multiplication not possible");
-        }
+            }
+        } else
+            System.out.println("Multiplication not possible");
+    }
 }
--- lab3&4/src/main/java/Lab4/magicSquare.java
+++ lab3&4/src/main/java/Lab4/magicSquare.java
@@ -8,6 +8,59 @@ package Lab4;
  *
  * @author student
  */
+//program to determine if matrix is magic square
+// sum for every row, the sum with every column, the sum of the principal diagonal and the sum of the non-principal diagonal elements.
+import java.util.Scanner;
+
 public class magicSquare {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean MS = true;
+        int sumr, refsum, sumc, sump, sumnp;
+        System.out.println("Enter dimension of square matrix: ");
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        System.out.println("Enter elements of matrix: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        // calc reference sum
+        refsum = 0;
+        for (int i = 0; i < n; i++)
+            refsum += arr[0][i];
+
+        
+        // checking sum of rows
+        for (int i = 0; i < n; i++) {
+            sumr = sumc = 0;
+            for (int j = 0; j < n; j++) {
+                sumr += arr[i][j];
+                sumc += arr[j][i];
+            }
+            if (sumr != refsum || sumc != refsum) {
+                System.out.println("sumr or sumc != refsum");
+                MS = false;
+            }
+        }
+
+        // calc sum of principal and non-principal diagonal
+        sump = sumnp = 0;
+        for (int i = 0; i < n; i++) {
+            sump += arr[i][i];
+            sumnp += arr[i][n - i - 1];
+        }
+
+        if (sump != refsum || sumnp != refsum) {
+            System.out.println("sump or sumnp != refsum");
+            MS = false;
+        }
+
+        if (MS == true)
+            System.out.println("Magic square");
+
+        else
+            System.out.println("Not magic square");
+    }
 }
