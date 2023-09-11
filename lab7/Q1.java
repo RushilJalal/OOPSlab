d18cd8a55cf7fe7248a0f57fb0c948d4d98aeaef
Rushil <rushiljalal@gmail.com>
2023-09-11 15:26:51 +0530

    Lab7

---
+++ lab7/Q1.java
@@ -0,0 +1,27 @@
+class Student_Detail
+{
+    String name;
+    int id;
+    String college_name =  "MIT";
+
+    Student_Detail(String n, int id)
+    {
+        name = n;
+        this.id = id;
+    }
+
+    void display_details()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Id: "+id);
+        System.out.println("College name: "+college_name);
+    }
+}
+
+class Q1{
+    public static void main(String[] args) {
+        //convert to array of objects and use scanner if time
+        Student_Detail stu1 = new Student_Detail("Rushil", 1234);
+        stu1.display_details();
+    }
+}
---
+++ lab7/Q3.java
@@ -0,0 +1,25 @@
+class Q3
+{
+    int i =10;
+    byte b = 20;
+    short s = 30;
+    long l = 40;
+    float f = 50.0;
+    double d = 60.0;
+
+    //autoboxing
+    Integer obj1 = new Integer(i);
+    Byte obj2 = new Byte(b);
+    Short obj3 = new Short(s);
+    Long obj4 = new Long(l);
+    Float obj5 = new Float(f);
+    Double obj6 = new Double(d);
+
+    //unboxing
+    System.out.println(obj1++);
+    System.out.println(obj2++);
+    System.out.println(obj3++);
+    System.out.println(obj4++);
+    System.out.println(obj5++);
+    System.out.println(obj6++);
+}
