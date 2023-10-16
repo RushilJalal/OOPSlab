54e2efc81532383f96ca0c34c293d291c2558dff
Rushil <rushiljalal@gmail.com>
2023-10-16 16:12:09 +0530

    comment out error

--- lab2/statements.java
+++ lab2/statements.java
@@ -3,12 +3,12 @@ class statements {
         int x = 10;
         double y = x;
         System.out.println(y);
-        //causes error
-        double x = 10.5;
-        int y = x;
-        System.out.println(y);
-        double x = 10.5;
-        int y = (int) x;
+        // causes error
+        // double x = 10.5;
+        // int y = x;
+        // System.out.println(y);
+        // double x = 10.5;
+        // int y = (int) x;
         System.out.println(y);
     }
 }
